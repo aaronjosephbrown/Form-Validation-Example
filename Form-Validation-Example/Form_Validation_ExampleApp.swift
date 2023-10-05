74c696edb7f72783ee87990be1579b16fba9d286
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-05 08:44:51 -0500

    Initial Commit

---
+++ Form-Validation-Example/Form_Validation_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_Validation_ExampleApp.swift
+//  Form-Validation-Example
+//
+//  Created by Aaron Brown on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Form_Validation_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
